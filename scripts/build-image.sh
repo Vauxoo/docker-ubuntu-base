635364cc06d46c6db6e47da0e18043d4ef1239a3
Luis Alejandro Martínez Faneyth <luis@huntingbears.com.ve>
2016-04-14 15:30:24 -0430

    [ADD] build-image.sh: Preliminary version of build script.

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,56 +1,84 @@
 #!/usr/bin/env sh
 
-# RUN locale-gen fr_FR \
-#     && locale-gen en_US.UTF-8 \
-#     && dpkg-reconfigure locales \
-#     && update-locale LANG=en_US.UTF-8 \
-#     && update-locale LC_ALL=en_US.UTF-8 \
-#     && ln -s /usr/share/i18n/SUPPORTED /var/lib/locales/supported.d/all \
-#     && locale-gen \
-#     && echo 'LANG="en_US.UTF-8"' > /etc/default/locale
-#
-# ENV PYTHONIOENCODING="utf-8" \
-#     LANG="C.UTF-8" \
-#     TERM="xterm"
-#
-# RUN apt-get update -q && apt-get install -qy wget \
-#     && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - \
-#     && echo 'deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main' > /etc/apt/sources.list.d/pgdg.list \
-#     && echo 'APT::Get::Assume-Yes "true";' >> /etc/apt/apt.conf \
-#     && echo 'APT::Get::force-yes "true";' >> /etc/apt/apt.conf \
-#     && apt-get update -q && apt-get upgrade -q \
-#     && apt-get install --allow-unauthenticated -q \
-#     bzr \
-#     curl \
-#     git \
-#     htop \
-#     libffi-dev \
-#     libpq-dev \
-#     libsasl2-dev \
-#     libssl-dev \
-#     locate \
-#     lsof \
-#     multitail \
-#     openssl \
-#     openssh-client \
-#     postgresql-client \
-#     postgresql-common \
-#     python \
-#     python-dev \
-#     python-psycopg2 \
-#     python-setuptools \
-#     supervisor \
-#     tmux \
-#     unzip \
-#     vim \
-#     vim-nox \
-#     w3m \
-#     wget \
-#     && cd /tmp \
-#     && wget -q https://bootstrap.pypa.io/get-pip.py \
-#     && python get-pip.py \
-#     && pip install --upgrade pyopenssl ndg-httpsclient pyasn1 \
-#     && pip install PyGithub click \
-#     && apt-get clean \
-#     && rm -rf /var/lib/apt/lists/* \
-#     && rm -rf /tmp/*
+# Where's my apt?
+APTGET="$( which apt-get )"
+
+# I'm not here, i won't interact with you
+APTGETCMD="env DEBIAN_FRONTEND=noninteractive ${APTGET}"
+
+# Let's tell apt to not install recommendations,
+# assume always a positive answer, and allow unauthenticated repos
+# Dpkg: you need to install configurations always. Ah, and be quiet.
+APTGETOPTS="-qq \
+            -o Apt::Install-Recommends=false \
+            -o Apt::Get::Assume-Yes=true \
+            -o Apt::Get::AllowUnauthenticated=true \
+            -o DPkg::Options::=--force-confmiss \
+            -o DPkg::Options::=--force-confnew \
+            -o DPkg::Options::=--force-overwrite \
+            -o DPkg::Options::=--force-unsafe-io"
+
+# List of software we need
+DPKGDEPENDS="bzr \
+             curl \
+             git \
+             htop \
+             locate \
+             lsof \
+             multitail \
+             openssl \
+             openssh-client \
+             postgresql-client \
+             postgresql-common \
+             python \
+             python-psycopg2 \
+             python-setuptools \
+             python-pip \
+             python-openssl \
+             python-ndg-httpsclient \
+             python-asn1 \
+             python-github \
+             python-click \
+             supervisor \
+             tmux \
+             unzip \
+             vim \
+             vim-nox \
+             w3m \
+             wget"
+
+# Blazing fast dpkg
+{
+	echo 'force-unsafe-io'
+} | sudo tee etc/dpkg/dpkg.cfg.d/speedup > /dev/null
+
+# Don't give me translations man, i can tok inglis
+{
+	echo 'Acquire::Languages "none";'
+} | sudo tee etc/apt/apt.conf.d/no-languages > /dev/null
+
+# This will setup our default locale.
+# Setting these three variables will ensure we have a proper locale environment
+# avoiding the use of PYTHONIOENCODING (See http://stackoverflow.com/a/34378962)
+locale-gen en_US.UTF-8
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+# Setting this according to vauxoo/docker-ubuntu-base#4
+export TERM="xterm"
+
+# Release the apt monster!
+${APTGETCMD} ${APTGETOPTS} update
+${APTGETCMD} ${APTGETOPTS} upgrade
+${APTGETCMD} ${APTGETOPTS} install ${DPKGDEPENDS}
+
+# Remove unnecessary files, we don't need you
+find usr -name "*.pyc" -print0 | xargs -0r sudo rm -rfv
+find var/cache/apt -type f -print0 | xargs -0r sudo rm -rfv
+find var/lib/apt/lists -type f -print0 | xargs -0r sudo rm -rfv
+find usr/share/man -type f -print0 | xargs -0r sudo rm -rfv
+find usr/share/doc -type f -print0 | xargs -0r sudo rm -rfv
+find usr/share/locale -type f -print0 | xargs -0r sudo rm -rfv
+find var/log -type f -print0 | xargs -0r sudo rm -rfv
+find var/tmp -type f -print0 | xargs -0r sudo rm -rfv
+find tmp -type f -print0 | xargs -0r sudo rm -rfv
